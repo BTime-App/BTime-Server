5394837935352f2c30eff8849e01210f130e7fde
hieptran <vanhiep99w@gmail.com>
2024-07-26 07:15:46 +0700

    Init project

---
+++ src/main/java/com/vn/project/btime/BTimeApplication.java
@@ -0,0 +1,13 @@
+package com.vn.project.btime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vn/project/btime/BTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.project.btime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
